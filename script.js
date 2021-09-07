a910301e7293371630080e1f11fc4ae3f47e73ba
Alinda Aulia Arniati <90227892+alindaarniati@users.noreply.github.com>
2021-09-07 19:02:13 +0700

    upload file web portfoliio
    WEB me :)

---
+++ script.js
@@ -0,0 +1,26 @@
+const txtElement = ['Pelajar', 'Murid', 'Calon Mahasiswi'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let wods = '';
+
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 500);	
+
+})();
